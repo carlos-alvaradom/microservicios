8d0d3bdc7405357363052dde12216e338e0f5652
unknown <carlos.alvarado@dcsas.com.co>
2023-10-30 11:43:04 -0500

    config server

---
+++ sipre-configservice-springboot/sipre-configservice-springboot/src/test/java/com/caam/sipre/configservice/SipreConfigserviceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caam.sipre.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipreConfigserviceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
