33189a3cd8c138e8afb30e69e39631aca145afa3
unknown <carlos.alvarado@dcsas.com.co>
2023-10-30 12:27:27 -0500

    eureka

---
+++ sipre-eurekaservice-springboot/sipre-eurekaservice-springboot/src/test/java/com/caam/sipre/eurekaservice/SipreEurekaserviceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caam.sipre.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipreEurekaserviceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
