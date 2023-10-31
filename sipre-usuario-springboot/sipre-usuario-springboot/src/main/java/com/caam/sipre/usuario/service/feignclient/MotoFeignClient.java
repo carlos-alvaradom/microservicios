e7e34d7d4d66cccd1f18cb24fa737d0a15d69704
unknown <carlos.alvarado@dcsas.com.co>
2023-10-31 10:10:43 -0500

    gateway

--- sipre-usuario-springboot/sipre-usuario-springboot/src/main/java/com/caam/sipre/usuario/service/feignclient/MotoFeignClient.java
+++ sipre-usuario-springboot/sipre-usuario-springboot/src/main/java/com/caam/sipre/usuario/service/feignclient/MotoFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "moto-service",url = "http://localhost:8280")
+@FeignClient(name = "moto-service")
 @RequestMapping("/moto")
 public interface MotoFeignClient {
 
