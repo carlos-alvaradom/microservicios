16bf31469553ddc3baa6c992e93aee9dc8f44357
unknown <carlos.alvarado@dcsas.com.co>
2023-10-31 09:45:58 -0500

    gateway

---
+++ sipre-gatewayservice-springboot/sipre-gatewayservice-springboot/src/test/java/com/caam/sipre/gatewayservice/SipreGatewayserviceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caam.sipre.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipreGatewayserviceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
